3e4d48b2db4fa8a91cc1c64d8b05d8fe279603e4
Cody.Diao <137848362+CodyDiao@users.noreply.github.com>
2023-11-14 21:45:32 -0700

    updating cprimer_simulator
    half way thru the cprimer module, creating save point

--- firmware_simulator/application/main.c
+++ firmware_simulator/application/main.c
@@ -22,14 +22,47 @@ All Global variable names shall start with "G_"
 Global variable definitions with scope limited to this local application.
 Variable names shall start with "Main_" and be declared as static.
 ***********************************************************************************************************************/
-
+static u8 Main_u8Servers = 0;          /* Number of people slinging drinks*/
 /***********************************************************************************************************************
 Main Program
 ***********************************************************************************************************************/
 
 void main(void)
 {
+  u32 u32UselessVariableForExample;
+  u32 au32BigArray[] = {5,4,3,2,1};
+  DrinkType aeDrinkArray[3] = {BEER, SHOOTER};
+  
+  u8 aString[] = "This is an array of u8 with NULL at end";
+  
+  /* Pointer example code */
+  u8 u8Test = 0xA5;
+  u32 u32Test = 0x0000ffff;
+  u8* pu8Example; 
+  u32* pu32Example;
+  
+  //Load the addresses into pointer variables
+  
+ pu8Example = &u8Test;
+ pu32Example = &u32Test;
+ 
+ //access the var via pointers (2 ways)
+ *pu8Example += 1;
+ (*pu32Example)++;
+ 
+ //moving the pointers (watch out for the second one!)
+ pu8Example++;
+ *pu32Example++;
 
+  //Struct example code
+ u8 u8CurrentServer;
+ ServerType sServer1;
+ ServerType* psServerParser;
+ 
+ psServerParser = &sServer1; //
+ sServer1.u8ServerNumber = 18; //assigns server number
+ u8CurrentServer = psServerParser -> u8ServerNumber; //gets the server # of current server
+  
   while(1)
   {
     
@@ -42,6 +75,32 @@ void main(void)
 * Function definitions
 ***********************************************************************************************************************/
 
+/*
+Function InitializeServer
+
+Description:
+Initializes a new server. A new server has an empty trya of drinks and is assigned the next 
+number available.
+
+Requires:
+  - psServer_ points to the server list where a new server is to be initialized;
+    since the the server list is a pointer toa linked list, this is a pointer-to-pointer
+  - Main_u8Servers hols the current number of active servers
+
+Promises:
+  - Returns TRUE if the server is initialized
+  - REturns FALSE if the server cannot be initialized
+*/
+
+bool InitializeServer(ServerType ** psServer_)
+{
+  if(*psServer_ == NULL)
+    return (FALSE); //when server cannot be initialized
+  //more code to be added
+  
+  return(TRUE);
+  
+} /* end InitializeServer() */
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* End of File */
--- firmware_simulator/application/main.h
+++ firmware_simulator/application/main.h
@@ -13,13 +13,19 @@ Header file for main.c.
 * Constant Definitions
 ***********************************************************************************************************************/
 #define U32_DRINK_SERVE_TIME    (u32)3000   /* Loop iterations before drink is removed */
-
+#define U8_MAX_DRINKS           (u8)3       /* This line type casts 3 as u8, and max drinks a server can hold*/
 
 /***********************************************************************************************************************
 * Type Definitions
 ***********************************************************************************************************************/
+typedef enum {EMPTY, BEER, SHOOTER, WINE, HIBALL} DrinkType;
 
-
+typedef struct
+{
+  u8 u8ServerNumber;                     //unique token for this item
+  DrinkType asServingTray[U8_MAX_DRINKS]; //data payload array
+  void* psNextServer;                     //pointer to the next ServerType
+}ServerType;
 /***********************************************************************************************************************
 * Function declarations
 ***********************************************************************************************************************/
